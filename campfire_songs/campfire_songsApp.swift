d9ab94401024b91bcf95c1f6ba99f375da311c98
joegriff19 <joegriff19@gmail.com>
2024-05-27 10:11:18 +0200

    Initial Commit

---
+++ campfire_songs/campfire_songsApp.swift
@@ -0,0 +1,17 @@
+//
+//  campfire_songsApp.swift
+//  campfire_songs
+//
+//  Created by Joseph Griffin on 27.05.24.
+//
+
+import SwiftUI
+
+@main
+struct campfire_songsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
